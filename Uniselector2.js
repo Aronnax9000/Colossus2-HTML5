4a23c70054c16658ce27fbb13bd30f56fde9a717
Alan Canon <aronnax9000@gmail.com>
2024-07-06 19:13:17 -0400

    Update Uniselector2.js

--- Uniselector2.js
+++ Uniselector2.js
@@ -1,11 +1,19 @@
 // uniselector2.js - Emulate a PO Type 2 Uniselector
 class Wiper {
+  constructor(props) {
+    this.name = props.name;
+    this.offset = props.offset;
+    this.bridging = props.bridging;
+  }
   offset;
   bridging = false;
 }
-class WiperLevel {
-   wipers;
+class Bank {
+   wipers; // Array of Wiper
 }
+
+
+
 
 class Uniselector2 {
 // 25 rows of wipers but 50 positions in 180 degree mode
