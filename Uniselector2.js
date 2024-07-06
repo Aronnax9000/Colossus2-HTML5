ccfc166246f7526bb658738c85b0b8ea8b62762e
Alan Canon <aronnax9000@gmail.com>
2024-07-06 17:42:13 -0400

    Create Uniselector2.js

---
+++ Uniselector2.js
@@ -0,0 +1,17 @@
+// uniselector2.js - Emulate a PO Type 2 Uniselector
+class Uniselector2 {
+// 25 rows of wipers but 50 positions in 180 degree mode
+  position = 0;
+  
+  name;
+  
+  constructor(props) {
+    this.name = props.name;
+    this.position = props.position;
+  }
+
+  introduceSelf() {
+    console.log('Uniselector2 ${this.name}');
+  }
+}
+
